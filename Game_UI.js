22b19262685276058cafe17a47ca52f91edd2751
CarterKerstetter <kerstettercarter7@gmail.com>
2020-03-24 19:47:04 -0400

    Row and col indicators now show correclty on chess board

--- Game_UI.js
+++ Game_UI.js
@@ -1,32 +1,58 @@
 class Game_UI{
+  FIRST_COLOR = 'white'
+  SECOND_COLOR = 'black'
+  BOARD_SIZE = 8
+
   constructor() {
     this.setupBoard()
   }
 
-  setupBoard() {
-    var board = document.getElementById('chess_board')
-    for( var x = 0 ; x < 8; x++ ) {
-      for( var y = 0 ; y < 8; y++ ) {
-        var square = document.createElement('img')
-        square.classList.add('chess_square')
-        var color = ( x + y ) % 2
-        if(color == 0) {
-          square.src = 'res/white.png'
-        }
-        else {
-          square.src = 'res/black.png'
-        }
-        board.appendChild(square)
-      }
-      var new_line = document.createElement('br')
-      board.appendChild(new_line)
+  createSquare( row, col ) {
+    let square = document.createElement( 'div' )
+    square.classList.add( 'chess_square' )
+    let color = ( row + col ) % 2
+    if(color == 0) {
+      square.style.background = this.FIRST_COLOR
     }
-    for ( var row = 1 ; row <= 8 ; row++ ) {
-      var row_indicator = document.createElement('div')
-      row_indicator.innerHTML = row.toString()
-      row_indicator.classList.add('row_indicator')
-      row_indicator.style.top = ((row-1) * 90/8 + 7.75).toString() + 'vw'
-      board.appendChild(row_indicator)
+    else {
+      square.style.background = this.SECOND_COLOR
+    }
+    return square
+  }
+
+  addRowNumber( row ) {
+    let row_indicator = document.createElement( 'div' )
+    row_indicator.innerHTML = ( row + 1 ).toString()
+    row_indicator.classList.add( 'row_indicator' )
+    row_indicator.style.top = ( row * 90/8 + 7.75 ).toString() + 'vw'
+    return row_indicator
+  }
+
+  addColLetter( col ) {
+    let col_indicator = document.createElement( 'div' )
+    col_indicator.innerHTML = String.fromCharCode( 97 + col )
+    col_indicator.classList.add( 'col_indicator' )
+    col_indicator.style.left = ( col * 90/8 + 7.75 ).toString() + 'vw'
+    return col_indicator
+  }
+
+  setupBoard() {
+    let board = document.getElementById( 'chess_board' )
+    for( let row = 0 ; row < this.BOARD_SIZE; row++ ) {
+      for( let col = 0 ; col < this.BOARD_SIZE; col++ ) {
+        // add letters for columns
+        if( row == 0 ) {
+          let col_indicator = this.addColLetter( col )
+          board.appendChild( col_indicator )
+        }
+        // add the squares for the board
+        let square = this.createSquare( row, col )
+        board.appendChild( square )
+      }
+      board.appendChild( document.createElement( 'br' ) )
+      // add numbers for the rows
+      let row_indicator = this.addRowNumber( row )
+      board.appendChild( row_indicator )
     }
   }
 }
